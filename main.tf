e5ab75ed7f665e775af12e76f11b1081be692756
Julian Wiegmann <julianwieg@users.noreply.github.com>
2022-10-02 14:29:45 +0200

    initial push

--- main.tf
+++ main.tf
@@ -1,30 +1,33 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.25.0"
+provider "google" {
+  project = "terraformproject-364311"
+  region  = "europe-north1"
+  zone    = "europe-north1-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
     }
   }
 
-  required_version = ">= 1.1.0"
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
 }
 
-provider "azurerm" {
-  features {}
-  version = "3.25.0"
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "westus2"
-}
 terraform {
   cloud {
     organization = "wiegme"
 
     workspaces {
-      name = "azure-playgroun"
+      name = "gcp-wiegme"
     }
   }
 }
