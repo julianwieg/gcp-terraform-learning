40e90551e262057f0dba59d4c5decffbafdb1b2d
Julian Wiegmann <julianwieg@users.noreply.github.com>
2022-10-02 14:45:12 +0200

    delete resource

--- main.tf
+++ main.tf
@@ -4,23 +4,7 @@ provider "google" {
   zone    = "europe-north1-a"
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "e2-micro"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    # A default network is created for all GCP projects
-    network = "default"
-    access_config {
-    }
-  }
-}
 
 terraform {
   cloud {
