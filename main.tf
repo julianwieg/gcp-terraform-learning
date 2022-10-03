bce78e2217c940bcd06456cc5a418cdca04559cf
Julian Wiegmann <julianwieg@users.noreply.github.com>
2022-10-03 17:06:23 +0200

    change made to folder setup

--- main.tf
+++ main.tf
@@ -7,19 +7,17 @@ terraform {
   }
 }
 
-/*
 provider "google" {
   credentials = var.GOOGLE_CREDENTIALS
 }
-
-data "google_folder" "production" {
-  folder = var.production_folder_name
-}
-*/
-
 
 resource "google_project" "production" {
   name       = "${var.team_name}Production"
   project_id = "${var.project_prefix}-${var.team_name}-prod"
   folder_id  = data.google_folder.production.name
+}
+
+
+data "google_folder" "production" {
+  folder = var.production_folder_name
 }
