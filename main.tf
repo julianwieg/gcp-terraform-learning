76aac42539ac04e3102bb95b7a5cb7377632374b
Julian Wiegmann <julianwieg@users.noreply.github.com>
2022-12-10 18:18:02 +0100

    s

--- main.tf
+++ main.tf
@@ -11,32 +11,3 @@ provider "google" {
   credentials = var.GOOGLE_CREDENTIALS
 }
 
-data "google_folder" "production" {
-  folder = var.production_folder_id
-}
-
-data "google_folder" "shared" {
-  folder = var.shared_folder_id
-}
-
-data "google_folder" "development" {
-  folder = var.development_folder_id
-}
-
-resource "google_project" "production" {
-  name       = "${var.team_names}Production"
-  project_id = "${var.project_prefix}-${var.team_names}-prod"
-  folder_id  = data.google_folder.production.id
-}
-
-resource "google_project" "development" {
-  name       = "${var.team_names}Development"
-  project_id = "${var.project_prefix}-${var.team_names}-dev"
-  folder_id  = data.google_folder.development.id
-}
-
-resource "google_project" "shared" {
-  name       = "${var.team_names}Shared"
-  project_id = "${var.project_prefix}-${var.team_names}-shared"
-  folder_id  = data.google_folder.production.id
-}
