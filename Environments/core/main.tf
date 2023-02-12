de72fa3615d8419b7dc361f79a1920047cb08683
Julian Wiegmann <julianwieg@users.noreply.github.com>
2023-02-12 14:36:33 +0100

    s

--- Environments/core/main.tf
+++ Environments/core/main.tf
@@ -18,9 +18,10 @@ module "folders" {
   parent  = "folders/608604741391"
 
   names = [
-    "dev",
+    "development",
     "staging",
     "production",
+    "common",
   ]
 
   set_roles = true
@@ -36,4 +37,21 @@ module "folders" {
   all_folder_admins = [
     "group:gcp-organization-admins@wieg.me",
   ]
+}
+
+module "log_export" {
+  source                 = "terraform-google-modules/log-export/google"
+  destination_uri        = "${module.destination.destination_uri}"
+  filter                 = ""
+  log_sink_name          = "storage_logsink"
+  parent_resource_id     = "608604741391"
+  parent_resource_type   = "folder"
+  unique_writer_identity = true
+}
+
+module "destination" {
+  source                   = "terraform-google-modules/log-export/google//modules/storage"
+  project_id               = "sample-project"
+  storage_bucket_name      = "storage_example_bucket"
+  log_sink_writer_identity = "${module.log_export.writer_identity}"
 }
