908cba492c4f7f8bf92b0e338dd3b993667360c3
Julian Wiegmann <julianwieg@users.noreply.github.com>
2023-02-12 13:46:45 +0100

    create core

---
+++ Enviroments/core/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3"
+    }
+  }
+}
+
+provider "google" {
+  credentials = var.GOOGLE_CREDENTIALS
+}
+
+module "folders" {
+  source  = "terraform-google-modules/folders/google"
+  version = "~> 3.0"
+
+  parent  = "folders/608604741391"
+
+  names = [
+    "dev",
+    "staging",
+    "production",
+  ]
+
+  set_roles = true
+
+  prefix = "fldr_"
+  
+  per_folder_admins = {
+    dev = "group:gcp-organization-admins@wieg.me"
+    staging = "group:gcp-organization-admins@wieg.me"
+    production = "group:gcp-organization-admins@wieg.me"
+  }
+
+  all_folder_admins = [
+    "group:gcp-organization-admins@wieg.me",
+  ]
+}
