52d25f5fefea0870c171ffa8261d95bfb094e9e7
Julian Wiegmann <julianwieg@users.noreply.github.com>
2023-02-19 17:26:20 +0100

    folders map output

---
+++ Environments/dev/main.tf
@@ -0,0 +1,30 @@
+// create random string for buckets
+resource "random_string" "suffix" {
+  length  = 4
+  upper   = false
+  special = false
+}
+
+data "terraform_remote_state" "gcp-core" {
+  backend = "remote"
+
+  config = {
+    organization = "wiegme"
+    workspaces = {
+      name = "gcp-core_Folders-shared_projects-org_policies-shared_network"
+    }
+  }
+}
+
+// setup dev project 
+module "dev_project" {
+  source                  = "terraform-google-modules/project-factory/google"
+  version                 = "~> 14.0"
+  random_project_id       = true
+  name                    = "development_project"
+  folder_id               = data.terraform_remote_state.gcp-core.outputs.folders_map["common"].name
+  org_id                  = var.organization_id
+  billing_account         = var.billing_account
+  default_service_account = "deprivilege"
+}
+
