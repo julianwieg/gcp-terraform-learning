d23bf7fbc53bea6818424d6827c9eee97ef7c7b2
Julian Wiegmann <julianwieg@users.noreply.github.com>
2023-02-19 16:46:29 +0100

    network

--- Environments/core/main.tf
+++ Environments/core/main.tf
@@ -66,3 +66,69 @@ module "destination" {
   log_sink_writer_identity = "${module.log_export.writer_identity}"
   force_destroy            = true
 }
+
+// setup our shared vpc and vpc project
+module "project-factory-network" {
+  source                  = "terraform-google-modules/project-factory/google"
+  version                 = "~> 14.0"
+  random_project_id       = true
+  name                    = "shared-network"
+  folder_id               = module.folders.folders_map["common"].name
+  org_id                  = var.organization_id
+  billing_account         = var.billing_account
+  default_service_account = "deprivilege"
+  
+  activate_apis = [
+    "compute.googleapis.com",
+    "iam.googleapis.com",
+  ]
+}
+
+module "vpc_dev" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 6.0"
+
+    project_id   = module.project-factory-network.project_id
+    network_name = "development-vpc"
+    routing_mode = "GLOBAL"
+    shared_vpc_host = "true"
+
+    delete_default_internet_gateway_routes = "true"
+
+    subnets = [
+        {
+            subnet_name           = "subnet-public"
+            subnet_ip             = "10.0.0.0/24"
+            subnet_region         = "us-west1"
+            description           = "Subnet for public access"
+        },
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.0.0/24"
+            subnet_region         = "us-west1"
+            description           = "This subnet 10.10.0.0/24 has a description"
+        },
+        {
+            subnet_name           = "subnet-node-01"
+            subnet_ip             = "10.20.0.0/24"
+            subnet_region         = "us-west1"
+            description           = "This subnet 10.20.0.0/24 has a description"
+        },
+        {
+            subnet_name           = "subnet-pods-01"
+            subnet_ip             = "192.168.0.0/24"
+            subnet_region         = "us-west1"
+            description           = "This subnet 192.168.0.0/24 has a description"
+        }
+    ]
+
+    routes = [
+        {
+            name                   = "egress-internet"
+            description            = "route through IGW to access internet"
+            destination_range      = "0.0.0.0/0"
+            tags                   = "egress-inet"
+            next_hop_internet      = "true"
+        }
+      ]
+}
